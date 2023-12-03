1854788f4ba9f5b53abce47f6cb7770f03b2a007
Durvesh27 <132134129+Durvesh27@users.noreply.github.com>
2023-12-03 22:25:25 +0530

    Updated

--- myntraclone/src/Components/Api Config/index.js
+++ myntraclone/src/Components/Api Config/index.js
@@ -3,12 +3,12 @@ import axios from "axios";
 const token = JSON.parse(localStorage.getItem("Token1"));
 if (token) {
   var api = axios.create({
-    baseURL: "http://localhost:8001/api/v1",
+    baseURL: "https://myntra-react-clone.onrender.com/api/v1",
     headers: { Authorization: `Bearer${token}` },
   });
 } else {
   var api = axios.create({
-    baseURL: "http://localhost:8001/api/v1",
+    baseURL: "https://myntra-react-clone.onrender.com/api/v1",
   });
 }
 export default api;
